94e4741c785721044844bc7f47dd2ea52ffe71ac
Vannes Wijaya <vannesoey@gmail.com>
2021-08-24 15:07:57 +0700

    query and mutation done, without cache

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="w-screen h-full flex flex-col items-center">
+  <div class="w-screen h-screen bg-yellow-300 flex flex-col items-center">
     <p class="text-5xl">DUKTIV</p>
+    <div class="flex flex-col gap-y-10">
+      <input v-model="title" />
+      <input v-model="due" />
+      <input v-model="status" />
+      <button @click="addProject({ title: title, due: due, status: status })">
+        ADD PROJECT
+      </button>
+    </div>
     <ul>
       <li v-for="project in projects" :key="project.id">
         <p class="text-3xl">{{ project.title }}</p>
+        <button>DELETE</button>
       </li>
     </ul>
   </div>
@@ -13,9 +22,16 @@
 import { ref } from "vue";
 import { useQuery, useResult, useMutation } from "@vue/apollo-composable";
 import queryProjects from "../graphql/projects.query.gql";
-import addProject from "../graphql/addProject.mutation.gql";
-import deleteProject from "../graphql/deleteProject.mutation.gql";
+import addProjectMutation from "../graphql/addProject.mutation.gql";
+import deleteProjectMutation from "../graphql/deleteProject.mutation.gql";
+
+const title = ref(null);
+const due = ref(null);
+const status = ref(null);
 
 const { result } = useQuery(queryProjects);
 const projects = useResult(result, null, (data) => data.projects);
+
+const { mutate: addProject } = useMutation(addProjectMutation);
+const { mutate: deleteProject } = useMutation(deleteProjectMutation);
 </script>
