dad56763cf4e79a9cb00a37477b024efa183c765
Vannes Wijaya <vannesoey@gmail.com>
2021-08-24 12:05:53 +0700

    client integrated with server successfully

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,38 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp, provide, h } from "vue";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
 
-createApp(App).use(router).mount('#app')
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+// const query = gql`
+//   query {
+//     projects {
+//       title
+//       due
+//       status
+//     }
+//   }
+// `;
+
+// defaultClient
+//   .query({
+//     query,
+//   })
+//   .then((res) => console.log(res));
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, defaultClient);
+  },
+  render() {
+    return h(App);
+  },
+})
+  .use(router)
+  .mount("#app");
